b600a40d801beea1d5403b76353f17e4218ef41a
jtbm37 <jtbm37@users.noreply.github.com>
2016-12-11 15:26:43 +0000

    Moved away from SQL.el Sql connection and temp buffer is now maintained internally.

--- sql-server.el
+++ sql-server.el
@@ -37,9 +37,29 @@
 (require 'ctable)
 
 (defconst sql-server-temp-buffer " *TEMP SQL*")
+(defgroup sql-server nil
+  "Sql Server"
+  :group 'SQL)
+
+(defcustom sql-server-ivy-columns-visible nil
+  "Whether the table columns are currently shown in minibuffer. Dictates the available action depending on whether tables or columns are shown"
+  :group 'sql-server)
+
+(defcustom sql-server-connection
+  '((login . nil)
+    (password . nil)
+    (server . nil)
+    (db . nil))
+  "sql-server connection details"
+  :group 'sql-server
+  :type 'alist)
+
+(defconst sql-server-ivy-table-columns-query "select COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '%s';"
+  "Defines the query to retrieve the columns of a table")
 
 (defvar sql-server-ivy-current-table nil
   "Holds current previously selected table when on columsn view")
+
 (defvar ivy--sql-table nil
   "Current table when completing columns")
 
@@ -49,42 +69,39 @@
 (defvar sql-server-tables-cache nil
   "Holds a cache of the database tables")
 
-(defvar sql-server-ivy-columns-visible nil
-  "Whether the table columns are currently shown in minibuffer. Dictates the available action depending on whether tables or columns are shown")
-
-(defconst sql-server-ivy-table-columns-query "select COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '%s';"
-  "Defines the query to retrieve the columns of a table")
-
-(defun sql-server-comint (product options)
-  "Create comint buffer and connect to Sql Server.
-Command is to `isql DB username password'"
-  (let ((params
-         (append
-          (if (not (string= "" sql-database))
-              (list sql-database))
-          (if (not (string= "" sql-user))
-              (list sql-user))
-          (if (not (string= "" sql-password))
-              (list sql-password))
-          options)))
-    (sql-comint product params)))
+;; (defun sql-server-comint (product options)
+;;   "Create comint buffer and connect to Sql Server.
+;; Command is to `isql DB username password'"
+;;   (let ((params
+;;          (append
+;; 	  (list "-S" sql-server)
+;;           (if (not (string= "" sql-database))
+;;               (list "-d" sql-database))
+;;           (if (not (string= "" sql-user))
+;;               (list "-U" sql-user))
+;;           (if (not (string= "" sql-password))
+;;               (list "-P" sql-password))
+;;           options)))
+;;     (sql-comint product params)))
 
 
 (advice-add 'sql-send-region :override #'sql-server-send-region)
 
-(sql-set-product-feature 'ms :sqli-program "isql")
+;; (sql-set-product-feature 'ms :sqli-program "sqlcmd")
 ;; nil will not ask for any input from user and get the defaults
-(sql-set-product-feature 'ms :sqli-login nil)
-(sql-set-product-feature 'ms :sqli-options (list "-n" "-x0x7c" "-c"))
-(sql-set-product-feature 'ms :sqli-comint-func 'sql-server-comint)
-(sql-set-product-feature 'ms :prompt-regexp "[0-9] SQL> ")
-(sql-set-product-feature 'ms :prompt-length 7)
-(sql-set-product-feature 'ms :terminator ";")
-(sql-set-product-feature 'ms :input-filter nil)
+;; (sql-set-product-feature 'ms :sqli-login nil)
+;; (sql-set-product-feature 'ms :sqli-options (list "-W" "-s" "	"))
+;; (sql-set-product-feature 'ms :sqli-options nil)
+;; (sql-set-product-feature 'ms :sqli-comint-func 'sql-server-comint)
+;; (sql-set-product-feature 'ms :prompt-regexp "[0-9]> ")
+;; (sql-set-product-feature 'ms :prompt-length 3)
+;; (sql-set-product-feature 'ms :terminator "go")
+;; (sql-set-product-feature 'ms :input-filter nil)
 
 (defun sql-server-input-filter (input)
   "Removes GO from input"
-  (replace-regexp-in-string "^go" ";" input))
+  ;; (replace-regexp-in-string "^go" ";" input)
+  )
 
 (defun sql-server-set-defaults ()
   "Sets default sql variable using `auth-source'.
@@ -94,17 +111,47 @@ machine sqllocal login `yourlogin' db `yourdatabase' password `yourpassword'
   (interactive)
   (-if-let* ((entry (nth 0 (auth-source-search :max 1 :host "sqllocal")))
          (login (plist-get entry :user))
+         (server (plist-get entry :server))
          (db (plist-get entry :db))
          (secretfun (plist-get entry :secret))
          (secret (funcall secretfun)))
-      (progn (setq sql-user login
-                   sql-password secret
-                   sql-database db
-                   sql-server db
-                   sql-send-terminator t
-                   sql-product 'ms)
-             (message "Sql-server defaults set"))
+      (progn
+	(setcdr  (assoc 'login sql-server-connection) login)
+	(setcdr  (assoc 'password sql-server-connection) secret)
+	(setcdr  (assoc 'db sql-server-connection) db)
+	(setcdr  (assoc 'server sql-server-connection) server)
+	(message "sql-server connection set"))
     (user-error "Credentials not found")))
+
+
+(defun sql-server--command-args ()
+  "Returns sqlcmd args preformatted for process call"
+  (list "-S" (cdr (assoc 'server sql-server-connection))
+	"-U" (cdr (assoc 'login sql-server-connection))
+	"-P" (cdr (assoc 'password sql-server-connection))
+	"-d" (cdr (assoc 'db sql-server-connection))
+	"-s" "\^E"
+	"-W"))
+
+(defun sql-server-connect ()
+  "Establishes connection to database specified in `sql-server-connection'"
+  (interactive)
+  (let ((process (apply 'start-process ;; dino
+		  "sql-server"
+		  sql-server-temp-buffer
+		  "sqlcmd"
+		  (sql-server--command-args))))
+    (set-process-query-on-exit-flag process nil)
+    (set-process-sentinel
+     process
+     (lambda (proc change)
+       (when (string-match "\\(finished\\|exited\\|exited abnormally with code\\)" change)
+	 (kill-buffer (process-buffer proc))
+	 (message (concat (process-name proc) " exited")))))
+    ;; (list process
+    ;; 	  (process-buffer process)
+    ;; 	  connection-info)
+    ))
 
 (defun sql-server-get-table-at-point (start end)
   (interactive "r")
@@ -133,29 +180,66 @@ When `nocount' is t, the last line with the row count is excluded."
 
 (defun sql-server-sanitize-query (sql)
   ;; Remove the GO statements and replace them with ';'
-  (let ((sql-fixed (s-trim
-                    (replace-regexp-in-string "\n" " "
-                                              ;;remove GOs'
-                                              (replace-regexp-in-string "^go\w*$" ""
-                                                                        ;; delete comments
-                                                                        (replace-regexp-in-string "^--.*$" "" sql))))))
-    (unless (s-suffix? ";" sql-fixed)
-      (setq sql-fixed (concat sql-fixed ";")))
-    sql-fixed))
+  ;; (let ((sql-fixed (s-trim
+  ;;                   (replace-regexp-in-string "\n" " "
+  ;;                                             ;;remove GOs'
+  ;;                                             (replace-regexp-in-string "^go\w*$" ""
+  ;;                                                                       ;; delete comments
+  ;;                                                                       (replace-regexp-in-string "^--.*$" "" sql))))))
+  ;;   (unless (s-suffix? ";" sql-fixed)
+  ;;     (setq sql-fixed (concat sql-fixed ";")))
+  ;;   sql-fixed)
+  sql)
+
+(defun sql-server--execute-query (sql)
+  (let ((process (get-buffer-process sql-server-temp-buffer)))
+    ;; (when (string-match "\\(.*\\);[ \t]*" sql) (setq sql (match-string 1 sql)))
+    (with-current-buffer (process-buffer process)
+      (delete-region (point-min) (point-max))
+      ;; We add 3 here to exclude the prompt from the result
+      (let ((start (+ 3 (point-min)))
+	    (end-flg-regexp "(\\([0-9]+\\) rows affected)")
+	    end)
+	(goto-char (point-max))
+	(process-send-string process (format "%s ;\n" sql))
+	(process-send-string process "go\n")
+	(goto-char (point-min))
+	(while (not (re-search-forward end-flg-regexp nil t 1))
+	  (when (accept-process-output process sqlserver-timeout-wait-for-result 0 nil)
+	    (goto-char (point-min))))
+	(setq end (max start (1- (match-beginning 0))))
+	;; (message "Count %s" (match-string 1))
+	(buffer-substring-no-properties start end)))))
 
 (defun sql-server-get-result-list (sql)
   "Returns the result of the query in a list"
   (unless (s-contains? "*Minibuf" (format "%s" (current-buffer)))
     (message "Sending query: %s" sql))
-  (let ((raw-lines (sql-server-get-result-lines sql)))
-    (mapcar (lambda (x)
-              (split-string x "|"))
-            raw-lines)))
+  (let ((raw-result (sql-server--execute-query sql))
+	result row line-count)
+    (with-temp-buffer
+      (insert raw-result)
+      (setq line-count (count-lines (point-min) (point-max)))
+      (goto-char (point-min))
+      (when (> line-count 1)
+	(while (<= (line-number-at-pos) line-count)
+	  (setq row (split-string (buffer-substring-no-properties
+				   (point-at-bol) (point-at-eol)) "\^E" t))
+	  (setq result (append result (list row)))
+	  (forward-line))))
+    (when (and result (> (length result) 1))
+      (setcdr result (cddr result)))
+    result)
+  ;; (let ((raw-lines (sql-server-get-result-lines sql)))
+  ;;   (mapcar (lambda (x)
+  ;;             (split-string x "|"))
+  ;;           raw-lines))
+  )
 
-(defun sql-server-get-result-lines (sql)
-  "Returns a list of each output lines"
-  (with-current-buffer "*SQL*"
-    (comint-redirect-results-list sql "\\(.+\\)$" 1)))
+;; (defun sql-server-get-result-lines (sql)
+;;   "Returns a list of each output lines"
+;;   (with-current-buffer "*SQL*"
+;;     (comint-redirect-results-list sql "\\(.+\\)$" 1)))
 
 (defvar sql-server-map
   (let ((map (make-sparse-keymap)))
@@ -207,15 +291,16 @@ is set. "
   (interactive "r")
   (sql-server-tables (buffer-substring-no-properties start end)))
 
-(defun sql-server-tablep (x)
-  (string= (nth 3 x) "TABLE"))
+;; (defun sql-server-tablep (x)
+;;   (string= (nth 3 x) "TABLE"))
 
 (defun sql-server-get-tables (&optional input ok we)
   (ivy--reset-tables)
   (when (or (and current-prefix-arg (= (car current-prefix-arg) 4)) (not sql-server-tables-cache))
     (message "Refreshing tables")
-    (setq sql-server-tables-cache (sql-server-get-result-list "\\tables;")))
-  (->> sql-server-tables-cache (-filter 'sql-server-tablep) (-map (lambda (x) (nth 2 x)))))
+    (setq sql-server-tables-cache (-flatten (cdr (sql-server-get-result-list "SELECT name from sys.tables order by name")))))
+  ;; (->> sql-server-tables-cache (-filter 'sql-server-tablep) (-map (lambda (x) (nth 2 x))))
+  sql-server-tables-cache)
 
 (defun sql-server-ivy-tables ()
   (interactive)
@@ -256,8 +341,8 @@ is set. "
 (defun sql-server-ivy-get-table-columns (table)
   "Returns a list of the table columns"
   ;;TODO Cache it
-  (-map 'sql-server-ivy-format-table-column (-drop-last 1 (cdr (sql-server-get-result-list (format "select COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '%s';"
-                                                                                     table))))))
+  (-map 'sql-server-ivy-format-table-column (cdr (sql-server-get-result-list (format "select COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '%s';"
+										     table)))))
 (defun sql-server-ivy-format-table-column (col)
   "Formats ivy table columns"
   (let* ((name (car col))
